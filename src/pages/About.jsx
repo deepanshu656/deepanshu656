a68a470faae6c103802bb41389756bf20e248eee
deepanshu656 <166552846+deepanshu656@users.noreply.github.com>
2024-08-03 23:35:29 +0530

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>GitHub Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        GitHub Finder is a sleek and intuitive React web app that lets you effortlessly search and discover GitHub users. 
+        Explore user profiles, repositories, and key information with ease. Simplify your GitHub exploration experience with GitHub Finder.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version : <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Developed By :&nbsp;
+        <a className='text-white' href='https://www.linkedin.com/in/asit-kumar-sahoo-1148211bb/'>
+          Asit Kumar Sahoo
+        </a>
+      </p>
+    </div>
+  )
+}
+
+export default About
